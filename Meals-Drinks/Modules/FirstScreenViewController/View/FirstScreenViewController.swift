1804643ce34b61a696117f5e46ee6c819f223201
SerhiiPavlichuk <S_Pavlichuk@icloud.com>
2021-09-11 12:41:46 +0300

    FirstScreenViewController changes

--- Meals-Drinks/Modules/FirstScreenViewController/View/FirstScreenViewController.swift
+++ Meals-Drinks/Modules/FirstScreenViewController/View/FirstScreenViewController.swift
@@ -10,8 +10,28 @@ import UIKit
 
 class FirstScreenViewController: UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var imageView: UIImageView = {
+            let imageView = UIImageView(frame: .zero)
+            imageView.image = UIImage(named: "LogoFirstPage")
+            imageView.contentMode = .scaleToFill
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            return imageView
+        }()
+        
+        view.insertSubview(imageView, at: 0)
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
-           super.viewWillAppear(animated)
+        super.viewWillAppear(animated)
         
         self.navigationController?.isNavigationBarHidden = true
     }
